bd6f885813a81b19a63464d0c4f618189c0e8279
Sayan Sinha <sayansinha5@gmail.com>
2022-01-01 23:59:44 +0530

    Created Dynamic Tables

--- src/app/bigocheatsheet/bigocheatsheet.component.ts
+++ src/app/bigocheatsheet/bigocheatsheet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SortingAlgo } from '../shared/sortingalgo';
 
 @Component({
   selector: 'app-bigocheatsheet',
@@ -7,6 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigocheatsheetComponent implements OnInit {
 
+  sortingAlgoList: SortingAlgo[] = [
+    { 
+      name: 'Insertion Sort',
+      sc_worst: 'O(1)',
+      tc_best: 'O(n)',
+      tc_average: 'O(n2)',
+      tc_worst: 'O(n2)' 
+    },
+    { 
+      name: 'Selection Sort',
+      sc_worst: 'O(1)',
+      tc_best: 'O(n2)',
+      tc_average: 'O(n2)',
+      tc_worst: 'O(n2)' 
+    },
+    {
+      name: 'Bubble Sort',
+      sc_worst: 'O(1)',
+      tc_best: 'O(n)',
+      tc_average: 'O(n2)',
+      tc_worst: 'O(n2)'
+    },
+    {
+      name: 'Shell Sort',
+      sc_worst: '',
+      tc_best: '',
+      tc_average: '',
+      tc_worst: ''
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/shared/sortingalgo.ts
@@ -0,0 +1,7 @@
+export class SortingAlgo {
+    name: string;
+    sc_worst: string;
+    tc_best: string;
+    tc_average: string;
+    tc_worst: string;
+}
